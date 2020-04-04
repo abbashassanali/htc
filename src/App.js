711f41d72f5fa08670b8b9970fb32a91d83e9c19
Abbas Hassanali <abbas.hassanali@jayway.com>
2020-04-04 10:19:41 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Page from './Page';
+import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    min-height: 100vh;
+    background: linear-gradient(to bottom, #000 54px, #007 96px);
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route key="/" path="/">
+            <Page />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Text = styled.p`
+  margin: 100px;
+  color: white;
+`
+function App() {
+  return (
+    <Text>Hej</Text>
+  );
+}
+
+export default App;
